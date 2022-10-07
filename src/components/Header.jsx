00a379c8abb38257ffbee674c7f6acb38e0c0de9
kareem nour <kimzcodes@gmail.com>
2022-10-06 22:36:33 -0700

    fix class name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <p class="fs-1">My Blog</p>
+      <p className="fs-1">My Blog</p>
       <div className="header">
         <ul>
           <li>Home</li>
